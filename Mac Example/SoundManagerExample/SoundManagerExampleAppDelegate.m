7e61312c7ac6d95cdc787da956061dbde68f50f9
demosthenese <nick@charcoaldesign.co.uk>
2011-03-31 15:33:00 +0000

    Fixed company name in Mac project

--- Mac Example/SoundManagerExample/SoundManagerExampleAppDelegate.m
+++ Mac Example/SoundManagerExample/SoundManagerExampleAppDelegate.m
@@ -3,7 +3,7 @@
 //  SoundManagerExample
 //
 //  Created by Nick Lockwood on 31/03/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Charcoal Design. All rights reserved.
 //
 
 #import "SoundManagerExampleAppDelegate.h"
